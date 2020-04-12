c97230e39f5ed98e29a0dcfd6d2ef3f529495c51
sagar <sagar@Sagars.local>
2020-04-12 15:55:06 +0530

    Files Updated

--- app/controllers/DistrictController.java
+++ app/controllers/DistrictController.java
@@ -3,36 +3,39 @@ package controllers;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import com.google.inject.Inject;
+
 import models.District;
 import play.libs.F;
 import play.libs.Json;
 import play.mvc.Result;
 import services.DistrictService;
 
-public class DistrictController extends BaseController{
-	
+public class DistrictController extends BaseController {
+
 	private final DistrictService districtService;
-	
+
 	@Inject
 	public DistrictController(DistrictService districtService) {
 		this.districtService = districtService;
 	}
-	
+
 	public F.Promise<Result> getAllDistrict() {
 		System.out.println("Calling Controller");
+
 		return F.Promise.promise(() -> {
-        Map<String, Object> hashMap = new HashMap<>();
-        List<District> district = districtService.getAllDistrict();
-        hashMap.put("District",district);
-        return ok(Json.toJson(hashMap));
+			Map<String, Object> hashMap = new HashMap<>();
+			List<District> district = districtService.getAllDistrict();
+			hashMap.put("District", district);
+			return ok(Json.toJson(hashMap));
 		});
-    }
- 	
+	}
+
 	public Result getAllDistrictsOrder() {
-        Map<String, Object> hashMap = new HashMap<>();
-        List<District> district = districtService.getAllDistrictsOrder();
-        hashMap.put("District", district);
-        return ok(Json.toJson(hashMap));
-    }
+		Map<String, Object> hashMap = new HashMap<>();
+		List<District> district = districtService.getAllDistrictsOrder();
+		hashMap.put("District", district);
+		return ok(Json.toJson(hashMap));
+	}
 }
