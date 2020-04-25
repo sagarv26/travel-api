55aed9780fe3aa3ba3af97664e4238e9724397e2
sagar <sagar@Sagars.local>
2020-04-25 16:46:20 +0530

    Added Place Controller

--- app/binding/ServiceModule.java
+++ app/binding/ServiceModule.java
@@ -2,16 +2,18 @@ package binding;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
-import services.*;
-import services.impl.*;
+
+import services.DistrictService;
+import services.PlaceService;
+import services.impl.DistrictServiceImpl;
+import services.impl.PlaceServiceImpl;
 
 /**
  * Created by shreyas.hande on 12/10/17.
  */
-public class ServiceModule extends AbstractModule
-{
-    protected void configure()
-    {
-        bind(DistrictService.class).to(DistrictServiceImpl.class).in(Scopes.SINGLETON);
-    }
+public class ServiceModule extends AbstractModule {
+	protected void configure() {
+		bind(DistrictService.class).to(DistrictServiceImpl.class).in(Scopes.SINGLETON);
+		bind(PlaceService.class).to(PlaceServiceImpl.class).in(Scopes.SINGLETON);
+	}
 }
