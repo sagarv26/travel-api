53b55a2eece194d34db1bf1c8658d0b62b2bf21e
sagar <sagar@Sagars.local>
2020-05-24 21:19:01 +0530

    Added Place Values

--- app/models/PlaceValues.java
+++ app/models/PlaceValues.java
@@ -5,25 +5,33 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.avaje.ebean.SqlRow;
+
 import play.db.ebean.Model;
 
 @Entity
-@Table(name = "place_values")
+@Table(name = "place_info")
 public class PlaceValues extends Model {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-    @Column
-    public Long id_place;
-	
-	@Column
-    public String best_season;
+	@Column(name = "id_place")
+	public Long id_place;
 
-    @Column
-    public String near_by_place;
+	@Column(name = "best_season")
+	public String best_season;
 
-    @Column
-    public String image;
-	
+	@Column(name = "place_image")
+	public String place_image;
+
+	@Column(name = "place_info")
+	public String place_info;
+
+	public PlaceValues(SqlRow placeInfo) {
+		this.id_place = placeInfo.getLong("id_place");
+		this.best_season = placeInfo.getString("best_season");
+		this.place_info = placeInfo.getString("place_info");
+		this.place_image = placeInfo.getString("place_image");
+	}
 }
