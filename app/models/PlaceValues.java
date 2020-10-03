678fa30cbddbc115a904f6e7169a2e8b07ea863e
sagar <sagar@Sagars.local>
2020-10-03 20:14:43 +0530

    Values Updated

--- app/models/PlaceValues.java
+++ app/models/PlaceValues.java
@@ -28,10 +28,34 @@ public class PlaceValues extends Model {
 	@Column(name = "place_desc")
 	public String place_desc;
 
+	@Column(name = "timings")
+	public String timings;
+
+	@Column(name = "location")
+	public String location;
+
+	@Column(name = "insta_link")
+	public String insta_link;
+
+	@Column(name = "place_type")
+	public String place_type;
+
+	@Column(name = "trending")
+	public String trending;
+
+	@Column(name = "time_required")
+	public String time_required;
+
 	public PlaceValues(SqlRow placeInfo) {
 		this.id_place = placeInfo.getLong("id_place");
 		this.best_season = placeInfo.getString("best_season");
 		this.place_desc = placeInfo.getString("place_desc");
 		this.place_image = placeInfo.getString("place_image");
+		this.timings = placeInfo.getString("timings");
+		this.location = placeInfo.getString("location");
+		this.insta_link = placeInfo.getString("insta_link");
+		this.place_type = placeInfo.getString("place_type");
+		this.trending = placeInfo.getString("trending");
+		this.time_required = placeInfo.getString("time_required");
 	}
 }
